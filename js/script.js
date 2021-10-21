38f6484eba2628f84eb0416572fcfb7bc4386dc3
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2021-10-21 15:49:18 +0200

    discount applicated

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
-const km = 10;
-const age = 27;
+const km = 46;
+const age = 65;
 const priceKm = 0.21;
 
 let totalPrice = km * priceKm;
-console.log ('totale biglietto', totalPrice)
+console.log ('totale biglietto', totalPrice);
 
+const discount20 = ((km * priceKm) * 20) / 100;
+const discount40 = ((km * priceKm) * 40) / 100;
+
+
+if (age < 18){
+  totalPrice = (km * priceKm) - discount20;
+}
+
+if (age => 65){
+  totalPrice = (km * priceKm) - discount40;
+}
+
+console.log ('totale biglietto 2 cifre', totalPrice.toFixed(2));
